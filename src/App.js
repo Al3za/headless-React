bd6f97ba2242f1ed324ceb314b56a455c671bde6
sandro <akecakabro@gmail.com>
2022-09-13 17:20:30 +0200

    add dotenv

--- src/App.js
+++ src/App.js
@@ -7,20 +7,17 @@ function App() {
   const [datas,setData]=useState([])
   
    const parse=require('html-react-parser');
-  //`${process.env.REACT_APP_API_URL}`
   
   useEffect(()=>{
-    fetch('https://public-api.wordpress.com/rest/v1.1/sites/alessandrocalabro476768755.wordpress.com/posts')
+    fetch(process.env.REACT_APP_API_URL)
     .then(res=>res.json())
     .then(data=>setData(data.posts[0]))
   },[])
 
-  console.log(datas)
-
    return (
     <div className="content">
 
-      {datas.content && <h2> {parse(datas.content)} </h2>}
+      {datas.content &&  parse(datas.content)}
       
     </div>
   ); 
